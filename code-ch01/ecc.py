88b456314b9b7f7b387727ab9c72cc9747eccad7
JohasDavid <johas@Johass-Mac-mini.local>
2026-03-27 17:35:49 -0600

    initial commit

---
+++ code-ch01/ecc.py
@@ -0,0 +1,58 @@
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = f"Num {num} not in field range 0 to {prime - 1}"
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+        
+    def __repr__ (self):
+        return f"FieldElement_{self.prime}({self.num})"
+    
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+    
+    def __ne__(self, other):
+        if other is None:
+            return False
+        return self.num != other.num or self.prime != other.prime
+    
+    def __add__ (self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in diffent Fields')
+        
+        num = ( self.num + other.num ) % self.prime
+        return self.__class__ (num, self.prime)
+    
+    def __sub__ (self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot substract two numbers in diffent Fields')
+        
+        num = ( self.num - other.num ) % self.prime
+        return self.__class__ (num, self.prime)
+    
+    def __mul__ (self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in diffent Fields')
+        
+        num = ( self.num * other.num ) % self.prime
+        return self.__class__ (num, self.prime)
+    
+    def __pow__ (self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__ (num, self.prime)
+    
+    def __truediv__ (self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in diffent Fields')
+        
+        if other.num == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
+        
+        num = (self.num * pow(other.num, self.prime - 2, self.prime) ) % self.prime
+        return self.__class__(num, self.prime)
+        
